dc398f416a9abcc859e9c478e977f60a60f9ec52
Tuong Nguyen <nguyentrongtuong1993@yahoo.com>
2015-02-02 14:50:49 +0700

    Using Ajax to finish playtime1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -59,6 +59,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to store_url }
+      format.js { } #for playtime 1, use hide to hidden cart
       format.json { head :no_content }
     end
   end
